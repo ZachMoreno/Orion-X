944107be19b194825a9dd52fdaf18c8f221a0481
XachMoreno <xachmoreno@gmail.com>
2012-06-28 22:08:43 -0700

    Included if for populating editor

--- embeddededitor.js
+++ embeddededitor.js
@@ -140,8 +140,15 @@ function(require, mTextView, mKeyBinding, mTextStyler, mTextMateStyler, mHtmlGra
 	
 	editor.installTextView();
 	// if there is a mechanism to change which file is being viewed, this code would be run each time it changed.
-	var contentName = "sample.js";  // for example, a file name, something the user recognizes as the content.
-	var initialContent = "window.alert('this is some javascript code');  // try pasting in some real code";
+	
+	if (!editor) {
+		var contentName = "sample.js";  // for example, a file name, something the user recognizes as the content.
+		var initialContent = "window.alert('this is some javascript code');  // try pasting in some real code"
+	} else {
+		contentName = "crap.js";
+		initialContent = "bullshit";
+	}
+
 	editor.setInput(contentName, null, initialContent);
 	syntaxHighlighter.highlight(contentName, editor);
 	editor.highlightAnnotations();
