8f65f50c7ab6b1bd3e50e11d0343b30792ab9077
Zach Moreno <xachmoreno@gmail.com>
2012-07-01 14:54:24 -0700

    Resource.getContent

--- orionPanel.js
+++ orionPanel.js
@@ -8,14 +8,24 @@ chrome.devtools.panels.create('Orion', 'img/orion24.png', 'index.html', function
 	console.log('panel',JSON.stringify(panel),panel);
 
 	chrome.devtools.panels.setOpenResourceHandler(function (selectResource) {
-		// access to resources & content
+		// access resource
 		chrome.devtools.inspectedWindow.getResources(function (resource) {
 			if (!resource) {
 				console.log('no resource');
 			} else {
-				console.log(resource);
+				console.log('Resource: ' + resource);
 				return resource;
 			}
+			
+			//access content
+			devtools.inspectedWindow.Resource.getContent(function (content) {
+				if (!content) {
+					console.console.log('no content');
+				} else {
+					console.log('Content: ' + content);
+					return content;
+				}
+			});
 		});
 	});
 });
