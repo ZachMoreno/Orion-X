8ce7f10a9f3a43d54762cc9ab3e71987ee0c00b0
Zach Moreno <xachmoreno@gmail.com>
2012-07-02 18:38:17 -0700

    Fixed indents WIP

--- requirePaths.js
+++ requirePaths.js
@@ -8,16 +8,16 @@ window.addEventListener('load', function onLoad() {
 
     function loadJSAsync() {
 		require({
-		paths: {
-			text: '/orion.client/bundles/org.eclipse.orion.client.core/web/requirejs/text',
-			i18n: '/orion.client/bundles/org.eclipse.orion.client.core/web/requirejs/i18n',
-			domReady: '/orion.client/bundles/org.eclipse.orion.client.core/web/requirejs/domReady',
-			orion: '/orion.client/bundles/org.eclipse.orion.client.editor/web/orion/',
-			examples: '/orion.client/bundles/org.eclipse.orion.client.editor/web/examples'
-		}
-	});
+			paths: {
+				text: '/orion.client/bundles/org.eclipse.orion.client.core/web/requirejs/text',
+				i18n: '/orion.client/bundles/org.eclipse.orion.client.core/web/requirejs/i18n',
+				domReady: '/orion.client/bundles/org.eclipse.orion.client.core/web/requirejs/domReady',
+				orion: '/orion.client/bundles/org.eclipse.orion.client.editor/web/orion/',
+				examples: '/orion.client/bundles/org.eclipse.orion.client.editor/web/examples'
+			}
+		});
 
-	/* Original copy of embeddededitor.js is in orion.client/bundles/org.eclipse.orion.client.editor/web/examples/editor/embeddededitor.js - embeddededitor.js in root is a copy to fix path issues */
-	require(["embeddededitor.js"]);
+		/* Original copy of embeddededitor.js is in orion.client/bundles/org.eclipse.orion.client.editor/web/examples/editor/embeddededitor.js - embeddededitor.js in root is a copy to fix path issues */
+		require(["embeddededitor.js"]);
     }
 });
